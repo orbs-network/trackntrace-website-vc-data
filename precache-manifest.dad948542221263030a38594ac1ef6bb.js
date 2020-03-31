f78ab016ca51fcb1a334d89feeda4319532c4a0f
aviram <aviram@orbs.com>
2020-03-31 10:56:09 +0300

    new format

---
+++ precache-manifest.dad948542221263030a38594ac1ef6bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "771ca67e86ddb4c9a00b5b284ef3e0df",
+    "url": "https://orbs-network.github.io/trackntrace-website-vc-data/index.html"
+  },
+  {
+    "revision": "d6e59fb2f7598a857c8f",
+    "url": "https://orbs-network.github.io/trackntrace-website-vc-data/static/css/main.9dd74186.chunk.css"
+  },
+  {
+    "revision": "521f9eb1be04350019fd",
+    "url": "https://orbs-network.github.io/trackntrace-website-vc-data/static/js/2.4e2d115c.chunk.js"
+  },
+  {
+    "revision": "662c5a7d52c991bfd2ab00183763cd1c",
+    "url": "https://orbs-network.github.io/trackntrace-website-vc-data/static/js/2.4e2d115c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6e59fb2f7598a857c8f",
+    "url": "https://orbs-network.github.io/trackntrace-website-vc-data/static/js/main.7bf53677.chunk.js"
+  },
+  {
+    "revision": "31e0e3a448936afaa43c",
+    "url": "https://orbs-network.github.io/trackntrace-website-vc-data/static/js/runtime-main.a68555d7.js"
+  }
+]);
